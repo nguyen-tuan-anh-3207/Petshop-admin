4471838188995c4fc3a0372372d9a4644b7d29d1
Tuan Anh <tuananhnguyen3207@gmail.com>
2022-02-23 16:55:22 +0700

    update authen

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import { Navigate, useRoutes } from 'react-router-dom';
+import { Navigate, Outlet, useRoutes } from 'react-router-dom';
 // layouts
 import DashboardLayout from './layouts/dashboard';
 import LogoOnlyLayout from './layouts/LogoOnlyLayout';
@@ -14,17 +14,27 @@ import NotFound from './pages/Page404';
 // ----------------------------------------------------------------------
 
 export default function Router() {
+  const isAuth = true;
   return useRoutes([
     {
       path: '/dashboard',
-      element: <DashboardLayout />,
+      element: isAuth ? <DashboardLayout /> : <Navigate to="/login" />,
       children: [
         { element: <Navigate to="/dashboard/app" replace /> },
         { path: 'app', element: <DashboardApp /> },
         { path: 'user', element: <User /> },
         { path: 'category', element: <Products /> },
         { path: 'products', element: <Products /> },
-        { path: 'blog', element: <Blog /> }
+        { path: 'order', element: <Products /> },
+        { path: 'blog', element: <Blog /> },
+        {
+          path: 'member',
+          element: <Outlet />,
+          children: [
+            { path: '/dashboard/member', element: <Products /> },
+            { path: '/dashboard/member/add', element: <Products /> }
+          ]
+        }
       ]
     },
     {
